24da9ef7cd53710c1717fa96866afa0fbb0a62e4
Aarón Gómez <nomeacuerdobien@gmail.com>
2025-03-06 21:31:20 +0100

    Preparacion de array

---
+++ app.js
@@ -0,0 +1,73 @@
+const products = [
+  {
+    name: 'MiniPc wall mount',
+    src: 'https://www.printables.com/model/1185570-minipc-wall-mount',
+    image: './assets/img/1.jpg'
+  },
+  {
+    name: 'Star Wars BD-1 Droid Kit',
+    src: 'https://www.printables.com/model/241744-star-wars-bd-1-droid-kit-card-individual-parts',
+    image: './assets/img/bd-1.jpg'
+  }
+  ,
+  {
+    name: 'R2 D2 aus Star Wars',
+    src: 'https://www.printables.com/model/610617-r2-d2-aus-star-wars',
+    image: './assets/img/4-e8362c23.jpg'
+  },
+  {
+    name: 'Star Wars toilet sign (use the force)',
+    src: 'https://www.printables.com/model/392559-star-wars-toilet-sign',
+    image: './assets/img/4-7242f212.jpg'
+  },
+  {
+    name: 'Star Wars Mandalorian Helmet',
+    src: 'https://www.printables.com/model/104677-how-to-print-the-mandalorian-helmet-size-s-m-l-and',
+    image: './assets/img/3-e8362c23.jpg'
+  },
+  {
+    name: 'Star Wars The Mandalorian Full Costume/Cosplay',
+    src: 'https://www.printables.com/model/527750-star-wars-the-mandalorian-full-costumecosplay-helm',
+    image: './assets/img/1-e8362c23.jpg'
+  },
+  {
+    name: 'Mandalorian Mythosaur',
+    src: 'https://www.printables.com/model/456347-mandalorian-mythosaur',
+    image: './assets/img/3-7242f212.jpg'
+  },
+  {
+    name: 'Ahsoka Tano\'s Lightsabers',
+    src: 'https://www.printables.com/model/421312-ahsoka-tanos-lightsabers-star-wars-the-mandalorian',
+    image: './assets/img/1-7242f212.jpg'
+  },
+  {
+    name: 'Cal Kestis\' Fallen Order Lightsaber',
+    src: 'https://www.printables.com/model/199721-cal-kestis-fallen-order-lightsaber-xenopixel-compa',
+    image: './assets/img/2-e8362c23.jpg'
+  },
+  {
+    name: 'Star Wars Pot Plant',
+    src: 'https://www.printables.com/model/494842-darth-vader-melted-mask-star-wars-pot-plant',
+    image: './assets/img/2-7242f212.jpg'
+  },
+  {
+    name: 'RC Formula 1',
+    src: 'https://www.printables.com/model/964784-fv01-the-most-advanced-3d-printed-rc-formula-1-car',
+    image: './assets/img/formula1.jpg'
+  },
+  {
+    name: 'SM-33 Head',
+    src: 'https://www.printables.com/model/1150954-sm-33-head-star-wars-skeleton-crew',
+    image: './assets/img/SM-33 Head.jpg'
+  },
+  {
+    name: 'The Goonies Magnet',
+    src: 'https://www.printables.com/model/1178180-the-goonies-magnet-multi-color8x3mm-magnets',
+    image: './assets/img/goo.webp'
+  }
+];
+
+/* codigo html
+  <ul class="gallery">             </ul>
+<li><img src="./assets/img/goo.webp"><a href="https://www.printables.com/model/1178180-the-goonies-magnet-multi-color8x3mm-magnets" rel="noopener"><h4>The Goonies Magnet</h4></a></li>*/
+
